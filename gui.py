3be07d71992e9fe404e594a946d5cec881356140
Tharakesh-coder <tharakeshrajesh3@gmail.com>
2025-08-06 14:22:53 -0500

    Created basic GUI

---
+++ gui.py
@@ -0,0 +1,19 @@
+import customtkinter as Ctk # Importing tkinter
+
+selecteddir = None
+
+# Functions
+def directoryselect():
+    global selecteddir
+    selecteddir = Ctk.filedialog.askdirectory()
+    print(selecteddir)
+
+app = Ctk.CTk() # Creating an app object
+app.geometry("400x500") # Setting the width and height of the app
+
+# Creating buttons and stuff
+selectdirbtn = Ctk.CTkButton(app, 100, 40, text="Select Directory to Scan", command=directoryselect)
+selectdirbtn.pack(padx=10, pady=10)
+
+if __name__ == '__main__':
+    app.mainloop()
